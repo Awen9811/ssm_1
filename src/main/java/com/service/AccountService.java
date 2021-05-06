c41d2105e1b5c37a216b01a92ed92f47bb83c7d6
AwenIY <66868205+xiaobai123-oss@users.noreply.github.com>
2021-05-06 16:59:23 +0800

    pull commit

--- src/main/java/com/service/AccountService.java
+++ src/main/java/com/service/AccountService.java
@@ -10,24 +10,8 @@ import java.util.List;
  * @create 2021-04-23 14:06
  */
 public interface AccountService {
-
-
-
-
-
-
     // 查询所有账户
     public List<Account> findAll();
-
-
-
-
-
-
     // 保存帐户信息
     public void saveAccount(Account account);
-
-
-
-
 }
